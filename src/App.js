8abe9d2170d9b93fc3c045e967c3be2925d17d07
Tim Holmes <tim@gravitaz.co.uk>
2019-12-17 16:42:16 +0000

    Feature change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Tim.
+          Feature Branch Change.
         </p>
         <a
           className="App-link"
